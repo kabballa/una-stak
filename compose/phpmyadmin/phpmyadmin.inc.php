5a8435da002bb6f189fd13917b43a06820e2f668
olariuromeo <olariu_romeo@yahoo.it>
2025-11-12 21:19:06 +0200

    ADD COMPOSE FILES

---
+++ compose/phpmyadmin/phpmyadmin.inc.php
@@ -0,0 +1,14 @@
+<?php
+
+$cfg['MaxRows'] = 50;
+$cfg['MaxTableList'] = 1500;
+$cfg['MaxNavigationItems'] = 1500;
+$cfg['NavigationDisplayLogo'] = false;
+
+$i = 0;
+
+/* Server: localhost [1] */
+$i++;
+$cfg['Servers'][$i]['verbose'] = 'local';
+$cfg['Servers'][$i]['host'] = 'mysql';
+
